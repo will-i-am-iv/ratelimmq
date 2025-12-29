688eaa6f3c64ae78a8b3ec25f49226ed8553bf3a
William Sutton IV <93950927+will-i-am-iv@users.noreply.github.com>
2025-12-28 21:57:24 -0800

    Update core.py

--- src/ratelimmq/handlers/core.py
+++ src/ratelimmq/handlers/core.py
@@ -4,14 +4,18 @@ from ratelimmq.context import Context
 from ratelimmq.protocol import Request, Response
 
 
-async def handle_ping(ctx: Context, req: Request) -> Response:
+async def ping(ctx: Context, req: Request) -> Response:
     return Response("PONG\n")
 
 
-async def handle_shutdown(ctx: Context, req: Request) -> Response:
+async def shutdown(ctx: Context, req: Request) -> Response:
     ctx.stop_event.set()
     return Response("BYE\n")
 
 
-async def handle_unknown(ctx: Context, req: Request) -> Response:
+async def help_cmd(ctx: Context, req: Request) -> Response:
+    return Response("OK commands: PING SHUTDOWN HELP\n")
+
+
+async def unknown(ctx: Context, req: Request) -> Response:
     return Response("ERR unknown command\n")
