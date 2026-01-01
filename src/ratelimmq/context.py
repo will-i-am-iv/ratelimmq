fea3e88751a470ee63a81e6efa1cdfc9d4110d39
William Sutton IV <93950927+will-i-am-iv@users.noreply.github.com>
2026-01-01 15:22:34 -0800

    Update context.py

--- src/ratelimmq/context.py
+++ src/ratelimmq/context.py
@@ -4,10 +4,12 @@ import asyncio
 from dataclasses import dataclass
 from typing import Any
 
+from ratelimmq.limiter import TokenBucket
+
 
 @dataclass
 class Context:
     stop_event: asyncio.Event
     cache: Any | None = None
     queue: Any | None = None
-    limiter: Any | None = None
+    limiter: TokenBucket | None = None
